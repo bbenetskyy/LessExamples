98ba70a017c9d1f6451e9d67c3328d0264e73138
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-04-03 11:40:29 +0200

    Add project files.

---
+++ LessExamples.Less/gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    less = require("gulp-less");
+
+
+gulp.task("Build Less Files", function () {
+    return gulp.src('*.less')
+        .pipe(less())
+        .pipe(gulp.dest('../LessExamples/LessExamples/Styles'));
+});
+
+
+gulp.task('default', ['Build Less Files']);
